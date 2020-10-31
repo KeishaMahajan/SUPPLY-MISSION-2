23c0c3e3cd74fdd774d450c08cd98dd5b9928e0b
KeishaMahajan <71004660+KeishaMahajan@users.noreply.github.com>
2020-10-31 12:47:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,41 +18,37 @@ function preload()
 
 function setup() {
 	createCanvas(800, 700);
-	engine=Engine.create ();
-	world=engine.world;
-
-	var ground_options=
-	{
-		isStatic:true
-	}
-
-
-
-	rectangle2= new Rectangle (500,610,20,100)
-	rectangle3= new Rectangle (300,610,20,100)
-	rectangle1= new Rectangle (400,650,200,20)
-
+	
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
-	World.add(world, packageBody);
-
-	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
-	ground=Bodies.rectangle(200,390,200,20,ground_options)
-	World.add(world,ground)
-
+	
 	helicopterSprite=createSprite(width/2, 200, 10,10);
 	helicopterSprite.addImage(helicopterIMG)
 	helicopterSprite.scale=0.6
 	
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+	
+	engine=Engine.create ();
+	world=engine.world;
+
+        packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
+	World.add(world, packageBody);
+
+	ground=Bodies.rectangle(200,390,200,20,ground_options)
+	World.add(world,ground)
+
+	rectangle2= new Rectangle (500,610,20,100)
+	rectangle3= new Rectangle (300,610,20,100)
+	rectangle1= new Rectangle (400,650,200,20)
+	
+	Engine.run(engine)
 }
 
 
 function draw() {
-	background ("black")
+background ("black")
 Engine.update(engine);
 packageSprite.x= packageBody.position.x 
 packageSprite.y= packageBody.position.y 
